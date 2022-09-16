8ed2c6dc0fb015549ffc76e33b02681087037ba9
Caleb Bareman <cbareman99@gmail.com>
2022-09-15 20:28:21 -0700

    Updated tests. Now failing some.

--- game/game_test.go
+++ game/game_test.go
@@ -11,6 +11,7 @@ type Position struct {
 	Nodes []int
 }
 
+// Testing positions from the Chess Programming Wiki: https://www.chessprogramming.org/Perft_Results
 func TestingPositions() []Position {
 	return []Position{
 		{
@@ -20,16 +21,16 @@ func TestingPositions() []Position {
 			Nodes: []int{20, 400, 8902, 4865609},
 		},
 		{
+			Name:  "Kiwipete",
+			Fen:   "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			Depth: []int{1, 2, 3, 5},
+			Nodes: []int{48, 2039, 97862, 1936900690},
+		},
+		{
 			Name:  "Endgame",
 			Fen:   "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
 			Depth: []int{1, 2, 3, 5},
 			Nodes: []int{14, 191, 2812, 674624},
-		},
-		{
-			Name:  "Castles",
-			Fen:   "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-			Depth: []int{1, 2, 3, 5},
-			Nodes: []int{48, 2039, 97862, 1936900690},
 		},
 		{
 			Name:  "Middle Game",
@@ -37,10 +38,21 @@ func TestingPositions() []Position {
 			Depth: []int{1, 2, 3, 5},
 			Nodes: []int{6, 264, 9467, 15833292},
 		},
+		{
+			Name:  "Talkchess",
+			Fen:   "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+			Depth: []int{1, 2, 3, 5},
+			Nodes: []int{44, 1486, 62_379, 89_941_194},
+		},
+		{
+			Name:  "Edwards 2",
+			Fen:   "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+			Depth: []int{1, 2, 3, 5},
+			Nodes: []int{46, 2_079, 89_890, 164_075_551},
+		},
 	}
 }
 
-// Get more positions to test here: https://gist.github.com/peterellisjones/8c46c28141c162d1d8a0f0badbc9cff9
 func TestFEN(t *testing.T) {
 	fenStrings := []string{
 		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
@@ -66,14 +78,14 @@ func TestMoves(t *testing.T) {
 	positions := TestingPositions()
 
 	for _, position := range positions {
-		t.Log(position.Name)
+		t.Logf("Testing %v\n", position.Name)
 		g, err := FromFEN(position.Fen)
 		if err != nil {
 			t.Errorf("Failed to create game with fen '%v'\n", position.Fen)
 			continue
 		}
 		for i, depth := range position.Depth {
-			if position.Nodes[i] > 10_000 {
+			if position.Nodes[i] > 100_000 {
 				t.Logf("Skipping depth %v. Too slow\n", depth)
 				break
 			}
@@ -85,43 +97,6 @@ func TestMoves(t *testing.T) {
 			}
 		}
 	}
-}
-
-func TestMovesLogged(t *testing.T) {
-	// positions := TestingPositions()
-
-	// for _, position := range positions {
-	// 	t.Log(position.Name)
-	// 	g, err := FromFEN(position.Fen)
-	// 	if err != nil {
-	// 		t.Errorf("Failed to create game with fen '%v'\n", position.Fen)
-	// 		continue
-	// 	}
-	// 	// extra move in e2e4 and g2g4
-	// 	perft := g.DividedPerft(3)
-	// 	for key, val := range perft {
-	// 		t.Logf("%v: %v\n", key, val)
-	// 	}
-	// }
-
-	g, _ := FromFEN("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")
-	perft := g.DividedPerft(2)
-	var sum int
-	for key, val := range perft {
-		sum += val
-		t.Logf("%v: %v\n", key, val)
-	}
-	t.Logf("%v Nodes searched\n", sum)
-
-}
-
-func TestRandom(t *testing.T) {
-	g, err := FromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")
-	if err != nil {
-		t.Logf("Error making game: %v\n", err)
-		return
-	}
-	t.Log(g.AllValidMoves())
 }
 
 func BenchmarkMoveGeneration(b *testing.B) {
